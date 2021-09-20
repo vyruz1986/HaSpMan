a410260332021c96762dde648ccc950b6290250f
Alex Goris <alex.goris@fastlikehell.com>
2021-09-20 20:16:51 +0200

    fix: Make sure forwarded headers are used

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -82,6 +82,8 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
