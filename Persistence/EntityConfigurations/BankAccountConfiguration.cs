f9258388e210cea770e84664f44cf14bde909f99
Alex Goris <alex.goris@fastlikehell.com>
2021-10-02 00:05:31 +0200

    feat: Added BankAccount domain, AddBankAccount command and handler

---
+++ Persistence/EntityConfigurations/BankAccountConfiguration.cs
@@ -0,0 +1,20 @@
+using System;
+
+using Domain;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Types;
+
+namespace Persistence.EntityConfigurations
+{
+    public class BankAccountConfiguration : IEntityTypeConfiguration<BankAccount>
+    {
+        public void Configure(EntityTypeBuilder<BankAccount> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("nvarchar").HasMaxLength(100);
+            builder.Property(p => p.AccountNumber).HasColumnType("varchar").HasMaxLength(34);
+        }
+    }
+}
