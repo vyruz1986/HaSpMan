8b185e1307923f1af0e75631619a538ae51ae79b
Berend Wouters <berend.wouters@gmail.com>
2021-10-05 22:08:12 +0200

    #3 Post merge bank account issues

--- Persistence/EntityConfigurations/BankAccountConfiguration.cs
+++ Persistence/EntityConfigurations/BankAccountConfiguration.cs
@@ -1,17 +1,13 @@
-using System;
-
 using Domain;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-using Types;
-
 namespace Persistence.EntityConfigurations
 {
     public class BankAccountConfiguration : IEntityTypeConfiguration<BankAccount>
     {
-        public void Configure(EntityTypeBuilder<BankAccount> builder)
+        public void Configure(EntityTypeBuilder<Domain.BankAccount> builder)
         {
             builder.Property(p => p.Name).HasColumnType("nvarchar").HasMaxLength(100);
             builder.Property(p => p.AccountNumber).HasColumnType("varchar").HasMaxLength(34);
