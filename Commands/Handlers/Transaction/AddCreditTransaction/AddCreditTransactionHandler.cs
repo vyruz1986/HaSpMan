5f7008195daea1a24a8a9776614613524e174d47
Berend Wouters <berend.wouters@gmail.com>
2021-10-10 19:54:33 +0200

    #3 Create and list transactions

---
+++ Commands/Handlers/Transaction/AddCreditTransaction/AddCreditTransactionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+using Persistence.Repositories;
+
+using Types;
+
+namespace Commands.Handlers.Transaction.AddCreditTransaction
+{
+    public class AddCreditTransactionHandler : IRequestHandler<AddCreditTransactionCommand, Guid>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public AddCreditTransactionHandler(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+        public async Task<Guid> Handle(AddCreditTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var lastSequence = await _transactionRepository.GetLastTransactionForBankAccount(request.BankAccountId);
+            var currentSequence = ++lastSequence;
+            var transaction = Domain.Transaction.CreateCreditTransaction(request.CounterPartyName, request.BankAccountId, request.Amount,
+                request.ReceivedDateTime, request.Description, currentSequence, new List<TransactionAttachment>(), request.MemberId, new List<TransactionTypeAmount>());
+            _transactionRepository.Add(transaction);
+            await _transactionRepository.SaveAsync(cancellationToken);
+            return transaction.Id;
+        }
+    }
+}
---
+++ Commands/Handlers/Transaction/AddDebitTransaction/AddDebitTransactionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+using Persistence.Repositories;
+
+using Types;
+
+namespace Commands.Handlers.Transaction.AddDebitTransaction
+{
+    public class AddDebitTransactionHandler : IRequestHandler<AddDebitTransactionCommand, Guid>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public AddDebitTransactionHandler(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+        public async Task<Guid> Handle(AddDebitTransactionCommand request, CancellationToken cancellationToken)
+        {
+
+            var lastSequence = await _transactionRepository.GetLastTransactionForBankAccount(request.BankAccountId);
+            var currentSequence = ++lastSequence;
+            var transaction = Domain.Transaction.CreateDebitTransaction(request.CounterPartyName, request.BankAccountId, request.Amount,
+                request.ReceivedDateTime, request.Description, currentSequence, new List<TransactionAttachment>(), request.MemberId, request.TransactionTypeAmounts);
+
+            _transactionRepository.Add(transaction);
+            await _transactionRepository.SaveAsync(cancellationToken);
+            return transaction.Id;
+        }
+    }
+}
