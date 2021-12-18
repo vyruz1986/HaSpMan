055cdbc22385cdc98e3f960cb9c9a1e2d8a336af
Joren Thijs <joren.thijs@hotmail.com>
2021-12-18 11:26:10 +0100

    fix: changed AutoCompleteCounterparty class to record

--- Queries/Members/Handlers/AutocompleteMember/AutocompleteCounterparty.cs
+++ Queries/Members/Handlers/AutocompleteMember/AutocompleteCounterparty.cs
@@ -2,14 +2,5 @@ using System;
 
 namespace Queries.Members.Handlers.AutocompleteMember
 {
-    public class AutocompleteCounterparty
-    {
-        public AutocompleteCounterparty(string name, Guid? memberId)
-        {
-            Name = name;
-            MemberId = memberId;
-        }
-        public string Name { get; set; }
-        public Guid? MemberId { get; set; }
-    };
+    public record AutocompleteCounterparty(string Name, Guid? memberId);
 }
