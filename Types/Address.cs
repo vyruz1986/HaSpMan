40dda8db02edc75981f7caade3db0c2cf716916d
Joren-Thijs <joren.thijs@hotmail.com>
2021-12-22 20:23:06 +0100

    fix: formatted Types project to net 6 standard

--- Types/Address.cs
+++ Types/Address.cs
@@ -1,17 +1,14 @@
-using System.ComponentModel.DataAnnotations;
+namespace Types;
 
-namespace Types
+public record Address(
+   string Street,
+   string City,
+   string Country,
+   string ZipCode,
+   string HouseNumber)
 {
-    public record Address(
-       string Street,
-       string City,
-       string Country,
-       string ZipCode,
-       string HouseNumber)
+    public override string ToString()
     {
-        public override string ToString()
-        {
-            return $"{Street} {HouseNumber}, {ZipCode} {City}, {Country}";
-        }
+        return $"{Street} {HouseNumber}, {ZipCode} {City}, {Country}";
     }
 }
