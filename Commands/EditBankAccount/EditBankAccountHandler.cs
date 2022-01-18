4d16cbdc309d404e8ef28066759907dd21b0b27a
Joren-Thijs <joren.thijs@hotmail.com>
2022-01-19 00:06:13 +0100

    Added EditBankAccountsCommand

---
+++ Commands/EditBankAccount/EditBankAccountHandler.cs
@@ -0,0 +1,35 @@
+using Commands.Extensions;
+using Commands.Services;
+
+using Domain.Interfaces;
+
+namespace Commands.EditBankAccount;
+
+public class EditBankAccountHandler : IRequestHandler<EditBankAccountCommand, Guid>
+{
+    private readonly IBankAccountRepository _repository;
+    private readonly IUserAccessor _userAccessor;
+
+    public EditBankAccountHandler(IBankAccountRepository repository, IUserAccessor userAccessor)
+    {
+        _repository = repository;
+        _userAccessor = userAccessor;
+    }
+
+    public async Task<Guid> Handle(EditBankAccountCommand request, CancellationToken cancellationToken)
+    {
+        var bankAccount = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        if (bankAccount is null)
+        {
+            throw new ArgumentException($"No bankaccount found for Id {request.Id}", nameof(request.Id));
+        }
+
+        var performingUser = _userAccessor.User.GetName()!;
+
+        bankAccount.ChangeName(request.Name, performingUser);
+        bankAccount.ChangeAccountNumber(request.AccountNumber, performingUser);
+
+        await _repository.SaveAsync(cancellationToken);
+        return bankAccount.Id;
+    }
+}
