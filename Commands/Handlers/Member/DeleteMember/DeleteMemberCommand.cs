b11e2b24e82d4a936a600f2262a6b710415aba6c
Berend Wouters <berend.wouters@gmail.com>
2022-06-28 12:29:07 +0200

    chore: remove SystemAuditTrailEventRepo #1

--- Commands/Handlers/Member/DeleteMember/DeleteMemberCommand.cs
+++ Commands/Handlers/Member/DeleteMember/DeleteMemberCommand.cs
@@ -1,12 +1,9 @@
 ﻿
 using Commands.Extensions;
-using Commands.Services;
 
 using Domain.Interfaces;
 
 using MediatR;
-
-using Persistence.Repositories;
 
 namespace Commands;
 
@@ -15,14 +12,10 @@ public record DeleteMemberCommand(Guid Id) : IRequest;
 public class DeleteMemberHandler : IRequestHandler<DeleteMemberCommand>
 {
     private readonly IMemberRepository _memberRepository;
-    private readonly IUserAccessor _userAccessor;
-    private readonly ISystemAuditEventRepository _systemAuditEventRepository;
 
-    public DeleteMemberHandler(IMemberRepository memberRepository, IUserAccessor userAccessor, ISystemAuditEventRepository systemAuditEventRepository)
+    public DeleteMemberHandler(IMemberRepository memberRepository)
     {
         _memberRepository = memberRepository;
-        _userAccessor = userAccessor;
-        _systemAuditEventRepository = systemAuditEventRepository;
     }
     public async Task<Unit> Handle(DeleteMemberCommand request, CancellationToken cancellationToken)
     {
@@ -30,19 +23,11 @@ public class DeleteMemberHandler : IRequestHandler<DeleteMemberCommand>
 
         if (member == null)
             throw new ArgumentException($"No member found by Id {request.Id}", nameof(request.Id));
-        var performingUser = _userAccessor.User.GetName()!;
-
+        
         _memberRepository.Remove(member);
 
-        var initalFirstName = member.FirstName[..1];
-        var intialLastName = member.LastName[..1];
-        var sanitzedMemberName = $"{initalFirstName}*** {intialLastName}***";
-
         await _memberRepository.Save(cancellationToken);
-
-        //_systemAuditEventRepository.Add(new Domain.SystemAuditEvent(DateTimeOffset.UtcNow, performingUser, 
-        //    $"Member {sanitzedMemberName} was deleted"));
-        //await _systemAuditEventRepository.SaveChangesAsync(cancellationToken);
+        
         return Unit.Value;
     }
 }
