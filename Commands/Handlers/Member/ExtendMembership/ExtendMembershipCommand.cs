e0c54308d7c2eb9269a893b8edcc9c87d5c2f3e8
Berend Wouters <berend.wouters@gmail.com>
2022-11-25 23:26:31 +0100

    chore: move handler to own file

--- Commands/Handlers/Member/ExtendMembership/ExtendMembershipCommand.cs
+++ Commands/Handlers/Member/ExtendMembership/ExtendMembershipCommand.cs
@@ -4,11 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Commands.Extensions;
-using Commands.Services;
-
-using Domain.Interfaces;
-
 using FluentValidation;
 
 namespace Commands.Handlers.Member.ExtendMembership;
@@ -22,33 +17,5 @@ public class ExtendMembershipCommandValidator : AbstractValidator<ExtendMembersh
         RuleFor(x => x.NewMembershipExpirationDate)
             .NotEmpty()
             .GreaterThan(DateTime.Now);
-    }
-}
-
-public class ExtendMembershipHandler : IRequestHandler<ExtendMembershipCommand>
-{
-    private readonly IMemberRepository _repository;
-    private readonly IUserAccessor _userAccessor;
-
-    public ExtendMembershipHandler(IMemberRepository repository, IUserAccessor userAccessor)
-    {
-        _repository = repository;
-        _userAccessor = userAccessor;
-    }
-
-    public async Task<Unit> Handle(ExtendMembershipCommand request, CancellationToken cancellationToken)
-    {
-        var member = await _repository.GetById(request.Id);
-
-        if (member == null)
-            throw new ArgumentException($"No member found by Id {request.Id}", nameof(request.Id));
-
-        var performingUser = _userAccessor.User.GetName()!;
-
-        member.ChangeMembershipExpiryDate(request.NewMembershipExpirationDate, performingUser);
-
-        await _repository.Save(cancellationToken);
-
-        return Unit.Value;
     }
 }
