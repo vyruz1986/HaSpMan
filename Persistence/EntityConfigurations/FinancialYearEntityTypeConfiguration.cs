e9b27f216063222deee59b82ad81d3758bfb7c26
Berend Wouters <berend.wouters@gmail.com>
2023-07-28 23:50:58 +0200

    feat: remove IsRequired and FinancialYearConfig

--- Persistence/EntityConfigurations/FinancialYearEntityTypeConfiguration.cs
+++ Persistence/EntityConfigurations/FinancialYearEntityTypeConfiguration.cs
@@ -9,9 +9,9 @@ public class FinancialYearEntityTypeConfiguration : IEntityTypeConfiguration<Fin
 {
     public void Configure(EntityTypeBuilder<FinancialYear> builder)
     {
-        builder.Property(x => x.StartDate).IsRequired();
-        builder.Property(x => x.EndDate).IsRequired();
-        builder.Property(x => x.IsClosed).IsRequired();
+        builder.Property(x => x.StartDate);
+        builder.Property(x => x.EndDate);
+        builder.Property(x => x.IsClosed);
         // Owned entity types cannot have inheritance hierarchies https://learn.microsoft.com/en-us/ef/core/modeling/owned-entities#current-shortcomings
         builder.HasMany(x => x.Transactions).WithOne();
         builder.Navigation(x => x.Transactions).AutoInclude(); 
