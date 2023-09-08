22302727e2159f43a2f21cadfb0e768e6ed906bd
Berend Wouters <berend.wouters@gmail.com>
2023-09-08 20:29:44 +0200

    test: move command tests to domain tests

---
+++ Domain.Test/FinancialYear/When_closing_a_financial_year.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Domain.Test.FinancialYear;
+
+public class When_closing_a_financial_year
+{
+    
+    public void It_should_mark_the_year_as_closed()
+    {
+        var startDate = DateTimeOffset.Now;
+        var financialYear = new Domain.FinancialYear(startDate, startDate.AddYears(1).AddDays(-1), new List<Domain.Transaction>());
+        
+;       financialYear.Close();
+        
+        financialYear.IsClosed.Should().BeTrue();
+    }
+}
