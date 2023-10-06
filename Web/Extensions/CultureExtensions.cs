ce4d1dbf4d24866db9a98e490e9fa42d847a582a
Alex Goris <alex.goris@fastlikehell.com>
2023-10-06 23:47:09 +0200

    fix: Add localization and show amounts in current ui culture everywhere

---
+++ Web/Extensions/CultureExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+using Microsoft.AspNetCore.Localization;
+
+namespace Web.Extensions;
+
+public static class CultureExtensions
+{
+    public static IServiceCollection AddLocalizationAndConfiguration(this IServiceCollection services)
+    {
+        services.AddLocalization();
+        return services;
+    }
+
+    public static IApplicationBuilder UseRequestLocalizationWithSupportedCultures(this IApplicationBuilder app)
+    {
+        var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures)
+            .Where(cul => !string.IsNullOrEmpty(cul.Name))
+            .ToArray();
+
+        var localizationOptions = new RequestLocalizationOptions()
+        {
+            DefaultRequestCulture = new RequestCulture("nl-BE"),
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures
+        };
+
+        app.UseRequestLocalization(localizationOptions);
+
+        return app;
+    }
+}
